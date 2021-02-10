64f3aacfdbc5332e49fbf7dccd54973d3b7d9266
dchancia <dchancia@purdue.edu>
2021-02-10 14:06:20 -0500

    update on extraction script

--- data_scripts/frame_ext.py
+++ data_scripts/frame_ext.py
@@ -11,22 +11,26 @@ r_path = r"F:\Users\user\Desktop\PURDUE\Research_Thesis\Thesis_Data\Burn_Data_Vi
 # Ultrasound frames writing path
 w_path = r"F:\Users\user\Desktop\PURDUE\Research_Thesis\Thesis_Data\Burn_Data_Frames_Classes\HUSD"
 
+
 def extract_frames_labels(video, count, write_path, dict):
+
     """ Extracts and saves every frame of the input video in the corresponding category (Full,
     partial, superficial). It returns the counter variable so the next time the function is
     called, the counter will start where it left. It returns the frame-labels dictionary to
     continue adding pairs when the function is called again"""
+
     cap = cv2.VideoCapture(video)
     ret = True
     try:
         while ret:
-            count += 1
             ret, frame = cap.read()
+            count += 1
             cv2.imwrite(os.path.join(write_path, "frame{}.jpg".format(count)), frame)
             dict["frame{}.jpg".format(count)] = video.split("\\")[9]
     except:
         print("Video is over")
     return count, dict
+
 
 # Initialize counter for frame extraction
 count_var = 0
@@ -39,11 +43,12 @@ for dirs in os.listdir(r_path):
     for subdir in os.listdir(os.path.join(r_path, dirs)):
         for filename in os.listdir(os.path.join(r_path, dirs, subdir)):
             file_path = os.path.join(r_path, dirs, subdir, filename)
-            print(file_path)
-            count_var, labels_dict = extract_frames_labels(file_path, count_var, os.path.join(w_path, dirs,), labels_dict)
+            count_var, labels_dict = extract_frames_labels(file_path, count_var, os.path.join(w_path, dirs),
+                                                           labels_dict)
+            print(count_var)
 
 # Create json file with frame-label pairs
-labels_json = json.dumps(labels_dict, indent=2)
-print(labels_json)
-with open(os.path.join(w_path, "labels.json"), "w") as json_file:
-    json.dump(labels_json, json_file)
+json_datafile = open("labels.json", "w")
+json.dump(labels_dict, json_datafile, indent=2)
+json_datafile.close()
+
