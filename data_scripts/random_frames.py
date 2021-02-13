c12f41e829c5fc76fb0fa91597318e30fad4c8f6
dchancia <dchancia@purdue.edu>
2021-02-13 13:21:10 -0500

    Extract random frames

--- data_scripts/random_frames.py
+++ data_scripts/random_frames.py
@@ -3,13 +3,13 @@ import random
 import shutil
 
 # Frames paths
-frames_path = r"F:\Users\user\Desktop\PURDUE\Research_Thesis\Thesis_Data\Burn_Data_Frames_Classes\HUSD"
-bal_frames_path = r"F:\Users\user\Desktop\PURDUE\Research_Thesis\Thesis_Data\Burn_Data_Frames_Balanced\HUSD"
+frames_path = r"F:\Users\user\Desktop\PURDUE\Research_Thesis\Thesis_Data\Burn_Data_Frames_Classes\TDI"
+bal_frames_path = r"F:\Users\user\Desktop\PURDUE\Research_Thesis\Thesis_Data\Burn_Data_Frames_Balanced\TDI"
 
 # Copy random files from original folder
 
 for dirs in os.listdir(frames_path):
-    subset = random.sample(os.listdir(os.path.join(frames_path, dirs)), 7500)
+    subset = random.sample(os.listdir(os.path.join(frames_path, dirs)), 6500)
     for file in subset:
         shutil.copyfile(os.path.join(frames_path, dirs, file), os.path.join(bal_frames_path, dirs, file))
 
